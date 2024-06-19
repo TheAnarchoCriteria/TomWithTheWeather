716c709df5daa1887cb0ac58c3a5a7aea9ffbe23
Manifesto <163677599+TheAnarchoCriteria@users.noreply.github.com>
2024-06-19 05:32:10 +0200

    Update resources_en.js

--- localizations/resources_en.js
+++ localizations/resources_en.js
@@ -8,7 +8,7 @@ const resources_en = {
     "Materials",
     "Care instructions",
     "Product journey",
-    "Why Do I Need a Blockchain Passport?",
+    "Why do I need a Blockchain Passport?",
     "Contact Me"
   ],
   info1: [
